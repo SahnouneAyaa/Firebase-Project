410522517411610298680e9f69ab05f567447112
sahnoune aya <sahnouneaya15@gmail.com>
2026-04-20 22:06:23 +0100

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB5v3fPuvXb1ojm0J9ubwVP64-3HK2i======c8Y",
   authDomain: "react-course-48f64.firebaseapp.com",
@@ -15,7 +11,6 @@ const firebaseConfig = {
   appId: "1:882013883752:web:31666282cf06bdb15cdadd",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
